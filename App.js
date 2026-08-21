cd98c35738fd9ed084ad1e8dddaf1894ec170480
Luciano Freire <lucianofreiredigital29@gmail.com>
2026-08-21 15:51:25 -0300

    feat: integra carrinho com Context API

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { CartProvider } from './src/contexts/CartContext';
 import HomeScreen from './src/screens/HomeScreen';
 import DetalheScreen from './src/screens/DetalheScreen';
+import CarrinhoScreen from './src/screens/CarrinhoScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Filmes' }}
-        />
-        <Stack.Screen
-          name="Detalhe"
-          component={DetalheScreen}
-          options={{ title: 'Detalhes do filme' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Filmes' }}
+          />
+          <Stack.Screen
+            name="Detalhe"
+            component={DetalheScreen}
+            options={{ title: 'Detalhes do filme' }}
+          />
+          <Stack.Screen
+            name="Carrinho"
+            component={CarrinhoScreen}
+            options={{ title: 'Carrinho' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 }
